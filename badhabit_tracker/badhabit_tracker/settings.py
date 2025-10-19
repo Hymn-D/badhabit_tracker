a48742c0c58b23d5fbaf5500a4fa8e92b5917038
esther <ogbonnaesther52@gmail.com>
2025-10-19 12:52:31 +0100

    add updates for deployment

--- badhabit_tracker/badhabit_tracker/settings.py
+++ badhabit_tracker/badhabit_tracker/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^2*x*mgsj-t!!nc(g=8ydee+7nvski%9ok+cj#aizms35z=qo8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.pythonanywhere.com']
+
 
 
 # Application definition
@@ -131,14 +132,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
 TEMPLATES[0]["DIRS"] = [BASE_DIR / "templates"]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
